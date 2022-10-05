5d2f7d44b99c4e4c63142f946aab0edb8dd45c93
Callum-Robson <cdrob@hotmail.com>
2022-10-05 19:55:29 -0400

    new repo with Challenge1 and Challenge2 projects

---
+++ GAME3121_A1_Challenge2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float timer = 0;
+    private float lastHit = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        lastHit += Time.deltaTime;
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (lastHit >= Time.deltaTime * 100)
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            }
+            lastHit = 0;
+        }
+    }
+}
